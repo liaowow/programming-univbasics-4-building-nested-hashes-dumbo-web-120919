c76a2235ec64b471b08697a4912bdcef90f0f4df
Annie Liao <liaowow@yahoo.com>
2019-11-13 16:41:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,9 +15,9 @@ end
 def monopoly_with_third_tier
 	base_hash = {
 	  :railroads => {
-	    :pieces => 4
+	    :pieces => 4,
+	    :rent_in_dollars => {}
 	  }
-	  :rent_in_dollars => {}
 	}
 end
 

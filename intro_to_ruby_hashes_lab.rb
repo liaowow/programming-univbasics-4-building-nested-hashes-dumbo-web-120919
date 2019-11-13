8c8e269240cdf409e45c4969c1b8c2de3bd8b2d2
Annie Liao <liaowow@yahoo.com>
2019-11-13 16:48:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
 	    :names => {
 	      :reading_railroad => {},
 	      :pennsylvania_railroad => {},
-	      :nkey3 => 3,
+	      :b_and_o_railroad => {},
 	      :nkey4 => 4
 	    }
 	  }
